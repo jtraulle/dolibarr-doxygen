2254b7a2d5bb19d6c0697631f37dcdcfb3fd4dd6
Jean Traullé <613615+jtraulle@users.noreply.github.com>
2019-08-29 00:56:02 +0200

    Update deployToGitHubPages.sh

--- deployToGitHubPages.sh
+++ deployToGitHubPages.sh
@@ -2,18 +2,14 @@
 
 GIT_DEPLOY_REPO=https://${GITHUB_API_KEY}@github.com/jtraulle/dolibarr-doxygen.git
 
-cd build
-git init
-
 git config user.name "Deployment Bot"
 git config user.email "deploy@travis-ci.org"
 
+git clone --single-branch --branch gh-pages ${GIT_DEPLOY_REPO} gh-pages
 
-for D in `find . -mindepth 1 -type d`
-do
-    echo "Adding '${D}' folder to git repository..."
-    git add ${D}/*
-done
+mv build_${VERSIONSFILE}/* gh-pages/
+
+git add .
 
 git commit -m "Deploy to GitHub Pages"
 git push --verbose --progress --force "${GIT_DEPLOY_REPO}" master:gh-pages

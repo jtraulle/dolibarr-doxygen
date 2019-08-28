10f56c6c81830de676505f5958590e3289f65bb5
Jean Traullé <613615+jtraulle@users.noreply.github.com>
2019-08-28 21:05:19 +0200

    Create deployToGitHubPages.sh

---
+++ deployToGitHubPages.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+GIT_DEPLOY_REPO=https://${GITHUB_API_KEY}@github.com/jtraulle/dolibarr-doxygen.git
+
+cd build && \
+git init && \
+git config user.name "Deployment Bot" && \
+git config user.email "deploy@travis-ci.org" && \
+git add . && \
+git commit -m "Deploy to GitHub Pages" && \
+git push --force "${GIT_DEPLOY_REPO}" master:gh-pages

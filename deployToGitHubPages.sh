da6c5d0e9b71f958768b5cd1b76a8a995fe6e039
Jean Traullé <613615+jtraulle@users.noreply.github.com>
2019-08-29 13:09:43 +0200

    Update deployToGitHubPages.sh

--- deployToGitHubPages.sh
+++ deployToGitHubPages.sh
@@ -2,7 +2,7 @@
 
 function bell() {
   while true; do
-    echo -e "."
+    echo -e "\a"
     sleep 60
   done
 }
@@ -10,22 +10,26 @@ bell &
 
 GIT_DEPLOY_REPO=https://${GITHUB_API_KEY}@github.com/jtraulle/dolibarr-doxygen.git
 
+echo -e "\nDisabling Git automatic garbage collecting and compression to save some time..."
 git config --global gc.auto 0
+git config --global core.compression 0
+git config --global core.preloadindex true
 
-mkdir gh-pages
+echo -e "\nDefining Git user name and user email..."
+git config --global user.name "Deployment Bot"
+git config --global user.email "deploy@travis-ci.org"
 
-mv build/* gh-pages/
-
-cd gh-pages
-
+echo -e "\nCreating new empty Git repository into 'build' directory..."
+cd build
 git init
 
-git config user.name "Deployment Bot"
-git config user.email "deploy@travis-ci.org"
-
+echo -e "\nAdding all Doxygen generated files to local Git repository..."
 git add . > /dev/null
 
+echo -e "\nCommitting added files to local Git repository..."
 git commit --quiet -m " Deploy jtraulle/dolibarr-doxygen to github.com/jtraulle/dolibarr-doxygen.git:gh-pages"
+
+echo -e "\nPushing local Git repository to remote location (GitHub) to deploy to GitHub Pages..."
 git push --force "${GIT_DEPLOY_REPO}" master:gh-pages
 
 exit $?

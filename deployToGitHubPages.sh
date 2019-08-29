628747225316109e44b867a62abefafaf004f35d
Jean Traullé <613615+jtraulle@users.noreply.github.com>
2019-08-29 10:39:30 +0200

    Update deployToGitHubPages.sh

--- deployToGitHubPages.sh
+++ deployToGitHubPages.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+function bell() {
+  while true; do
+    echo -e "\a"
+    sleep 60
+  done
+}
+bell &
+
 GIT_DEPLOY_REPO=https://${GITHUB_API_KEY}@github.com/jtraulle/dolibarr-doxygen.git
 
 git clone --single-branch --branch gh-pages ${GIT_DEPLOY_REPO} gh-pages
 
-mv build_${VERSIONSFILE}/* gh-pages/
+mv build/* gh-pages/
 
 cd gh-pages
 
@@ -15,3 +23,5 @@ git add . > /dev/null
 
 git commit --quiet -m " Deploy jtraulle/dolibarr-doxygen to github.com/jtraulle/dolibarr-doxygen.git:gh-pages"
 git push --force "${GIT_DEPLOY_REPO}" gh-pages:gh-pages
+
+exit $?

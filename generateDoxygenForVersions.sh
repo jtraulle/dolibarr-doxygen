e7744269ae8ae7e1ace7818439446958d9a14cc2
Jean Traullé <613615+jtraulle@users.noreply.github.com>
2019-08-30 13:16:17 +0200

    Update generateDoxygenForVersions.sh

--- generateDoxygenForVersions.sh
+++ generateDoxygenForVersions.sh
@@ -2,13 +2,6 @@
 
 DIRPATH="$(pwd)"
 
-cd dolibarr && git tag --sort=-v:refname > ../versions/dolibarrVersions
-cd ..
-cd versions && split -d -n l/10 dolibarrVersions versions
-cd ..
-
-echo "Using version file versions/$VERSIONSFILE"
-
 while read version; do
   echo "Generating Doxygen doc for Dolibarr version $version"
   echo ""
@@ -17,5 +10,4 @@ while read version; do
   git checkout --quiet -f $version
   cd ${DIRPATH}
   ( cat dolibarr.doxyfile ; echo "PROJECT_NUMBER=$version" ; echo "OUTPUT_DIRECTORY=build_$VERSIONSFILE" ; echo "HTML_OUTPUT=$version" ) | doxygen -
-done <versions/$VERSIONSFILE
-
+done <versions/dolibarrVersions

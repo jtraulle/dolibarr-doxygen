2485ba890ef2c0756f77e73804e7b073b009f143
Jean Traullé <jtraulle@opencomp.fr>
2019-09-01 14:11:48 +0200

    Trying without composer and after chmod +x

--- generateDoxygenForVersions.sh
+++ generateDoxygenForVersions.sh
@@ -3,20 +3,6 @@
 DIRPATH="$(pwd)"
 
 while read version; do
-  echo "Requiring hschletz/doxygen-phpdoc composer dependency..."
-  echo ""
-  composer require --prefer-dist hschletz/doxygen-phpdoc
-
-  ls
-  echo "---"
-  ls vendor/
-  echo "---"
-  ls vendor/bin
-  echo "---"
-  ls vendor/hschletz/doxygen-phpdoc
-  echo ""
-  echo ""
-  
   echo "Generating Doxygen doc for Dolibarr version $version"
   echo ""
 

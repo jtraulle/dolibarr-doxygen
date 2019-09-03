2598eba96f5b1d28aafbb90029c506f2dd9d2341
Jean Traullé <jtraulle@opencomp.fr>
2019-09-03 14:10:59 +0200

    Big refactoring (added PDF generation using Lua LaTeX) - Custom Docker image bundling compiled from sources and patched Doxygen 1.8.16 - Custom Docker image bundling Alpine Edge TeXLive distribution - Adding PDF generation using Lua LaTeX - Customized header.tex to use pdfencoding=auto option for hyperref LaTeX package

--- generateDoxygenForVersions.sh
+++ generateDoxygenForVersions.sh
@@ -16,19 +16,27 @@ cd ..
 echo "Using version file versions/$VERSIONSFILE"
 echo ""
 
-while read version; do
-  echo "Generating Doxygen doc (HTML and LaTeX) for Dolibarr version $version"
-  echo ""
-
+while IFS= read -r version; do
   cd dolibarr
+  echo "Checking out Git branch or tag '$version'"
+  echo ""
   git checkout --quiet -f $version
   cd ${DIRPATH}
-  ( cat dolibarr.doxyfile ; echo "PROJECT_NUMBER=$version" ; echo "OUTPUT_DIRECTORY=build_$VERSIONSFILE" ; echo "LATEX_OUTPUT=pdf_$version" ; echo "HTML_OUTPUT=$version" ) | doxygen -
 
-  echo "Generating Doxygen PDF doc (from LaTeX using LuaLaTeX) for Dolibarr version $version"
+  echo "Generating Doxygen HTML doc for Dolibarr version $version"
   echo ""
-  cd build_$VERSIONSFILE/pdf_$version/
-  make
+  docker run --user ${UID} --rm -v `pwd`:/home jtraulle/doxygen:1.8.16 /bin/sh -c "( cat dolibarr.doxyfile ; echo 'PROJECT_NUMBER=$version' ; echo 'OUTPUT_DIRECTORY=build_$VERSIONSFILE' ; echo 'HTML_OUTPUT=$version' ) | doxygen -"
+
+  echo "Generating Doxygen LaTeX doc for Dolibarr version $version"
+  echo ""
+  docker run --user ${UID} --rm -v `pwd`:/home jtraulle/doxygen:1.8.16 /bin/sh -c "( cat dolibarr.doxyfile ; echo 'PROJECT_NUMBER=$version' ; echo 'OUTPUT_DIRECTORY=build_$VERSIONSFILE' ; echo 'GENERATE_HTML=NO' ; echo 'GENERATE_LATEX=YES' ; echo 'LATEX_OUTPUT=pdf_$version' ) | doxygen -"
+
+  echo "Generating Doxygen PDF doc (using LuaLaTeX) for Dolibarr version $version"
+  echo ""
+  sed -i "s/(version)/(version $version)/g" build_$VERSIONSFILE/pdf_$version/refman.tex
+  sed -i "s/(version main page)/Version $version/g" build_$VERSIONSFILE/pdf_$version/refman.tex
+  docker run --rm -v `pwd`:/home jtraulle/doxygen:1.8.16 /bin/sh -c "cd build_$VERSIONSFILE/pdf_$version/ && make && chmod a+rw refman*"
   cd ${DIRPATH}
-  mv build_$VERSIONSFILE/pdf_$version/refman.pdf build_$VERSIONSFILE/$version/dolibarr-$version.pdf
-done <versions/$VERSIONSFILE
+  cp build_$VERSIONSFILE/pdf_$version/refman.pdf build_$VERSIONSFILE/$version/dolibarr-$version.pdf
+  rm -rf build_$VERSIONSFILE/pdf_$version
+done <"versions/$VERSIONSFILE"
